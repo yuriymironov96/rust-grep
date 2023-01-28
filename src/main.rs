997e14014e37c97589d56bd496c94a8ef4a50b65
Yurii Mironov <yuriymironov96@gmail.com>
2023-01-28 16:16:14 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+
+use rust_grep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_grep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
